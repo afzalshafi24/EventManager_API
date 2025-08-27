6b04a23fa06ad1fc159ff5f33fcdbab53f2b0aed
afzalshafi24 <afzalshafi24@gmail.com>
2025-08-27 16:56:12 -0700

    Add files via upload

---
+++ ECQL_Emulator/API/src/models.py
@@ -0,0 +1,20 @@
+
+from pydantic import BaseModel
+from typing import Optional
+
+# Define a Pydantic model for the request body for Event Alerts
+class Event_Request(BaseModel):
+    scid: int
+    event_time: str
+    event_rule_id: int
+    event_name: str
+    event_rule: str
+    event_src: str
+    spark_script: Optional[str]
+    gem_full_path: Optional[str]
+
+# Define a Pydantic model for the request body for Spark Requests
+class SparkRequest(BaseModel):
+    job_id: int
+    url: str
+    spark_id: int
